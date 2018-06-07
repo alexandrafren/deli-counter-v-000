7d198273baed3ae320a960dc019ca4f0f1253d0d
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-07 15:17:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length > 0
-   current_line = "The line is currently: "
+   current_line = "The line is currently:"
      katz_deli.each_with_index do |names, i|
-       current_line << "#{i + 1}. #{names} "
+       current_line << " #{i + 1}. #{names}"
   end
   puts current_line
 end
